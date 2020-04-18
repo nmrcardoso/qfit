4362c3f34beb6e9a42c2b634faacc261fc037c0c
nmrcardoso <nmrcardoso@gmail.com>
2020-04-18 19:24:59 +0100

    remove comment

--- qfit.cpp
+++ qfit.cpp
@@ -18,9 +18,6 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 
-
-//#define USE_FL_WIDGET_DRAW
-
 #include <iostream>
 #include <sstream>
 #include <fstream>
